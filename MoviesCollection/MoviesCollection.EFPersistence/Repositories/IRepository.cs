03b9226c69db935e1738534a264ae7e7fdb3bb53
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2022-02-24 21:33:04 -0300

    created the repository base

---
+++ MoviesCollection/MoviesCollection.EFPersistence/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesCollection.EFPersistence.Repositories
+{
+    public interface IRepository<TEntity>
+        where TEntity : class
+    {
+
+        IQueryable<TEntity> All { get; }
+        Task<TEntity> GetById(int id);
+        Task Add(TEntity entity);
+        Task Update(TEntity entity);
+        Task DeleteById(TEntity entity);
+
+    }
+}
---
+++ MoviesCollection/MoviesCollection.EFPersistence/Repositories/RepositoryBase.cs
@@ -0,0 +1,44 @@
+﻿using MoviesCollection.EFPersistence.Context;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesCollection.EFPersistence.Repositories
+{
+    public class RepositoryBase<TEntity>
+        : IRepository<TEntity>
+        where TEntity : class
+    {
+
+        private readonly SeriesMovieContext context;
+        public RepositoryBase(SeriesMovieContext db)
+        {
+            this.context = db;
+        }
+
+        public IQueryable<TEntity> All =>
+            this.context.Set<TEntity>().AsQueryable();
+
+        public async Task Add(TEntity entity)
+        {
+            await this.context.Set<TEntity>().AddRangeAsync(entity);
+            await this.context.SaveChangesAsync();
+        }
+
+        public async Task DeleteById(TEntity entity)
+        {
+            this.context.Set<TEntity>().RemoveRange(entity);
+            await this.context.SaveChangesAsync();
+        }
+
+        public async Task<TEntity> GetById(int id)
+        {
+            return await this.context.FindAsync<TEntity>(id);
+        }
+
+        public async Task Update(TEntity entity)
+        {
+            this.context.Set<TEntity>().UpdateRange(entity);
+            await this.context.SaveChangesAsync();
+        }
+    }
+}
