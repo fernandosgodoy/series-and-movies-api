025b46c1c31770bb77e41623b411269d312639d7
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2022-02-24 21:02:06 -0300

    changes

--- MoviesCollection/MoviesCollection.Api/Startup.cs
+++ MoviesCollection/MoviesCollection.Api/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MoviesCollection.EFPersistence.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,11 @@ namespace MoviesCollection.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<SeriesMovieContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("SeriesMovieContext"));
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
