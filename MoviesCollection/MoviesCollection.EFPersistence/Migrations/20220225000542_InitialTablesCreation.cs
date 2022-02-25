6faad3254c58cb976a48cb0397fa012a8a41f1bd
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2022-02-24 21:08:15 -0300

    created the migration file

---
+++ MoviesCollection/MoviesCollection.EFPersistence/Migrations/20220225000542_InitialTablesCreation.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoviesCollection.EFPersistence.Migrations
+{
+    public partial class InitialTablesCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "actors",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    birthdate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    biography = table.Column<string>(type: "nvarchar(800)", maxLength: 800, nullable: true),
+                    image_url = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    deleted = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_actors", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "movies",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(type: "nvarchar(70)", maxLength: 70, nullable: false),
+                    cover_url = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    storyline = table.Column<string>(type: "nvarchar(800)", maxLength: 800, nullable: true),
+                    languages = table.Column<int>(type: "int", nullable: false),
+                    release_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    synopsis = table.Column<string>(type: "nvarchar(max)", maxLength: 8000, nullable: true),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    deleted = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_movies", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "casts",
+                columns: table => new
+                {
+                    actor_id = table.Column<int>(type: "int", nullable: false),
+                    movie_id = table.Column<int>(type: "int", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_casts", x => new { x.actor_id, x.movie_id });
+                    table.ForeignKey(
+                        name: "FK_casts_actors_actor_id",
+                        column: x => x.actor_id,
+                        principalTable: "actors",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_casts_movies_movie_id",
+                        column: x => x.movie_id,
+                        principalTable: "movies",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "episodes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(type: "nvarchar(70)", maxLength: 70, nullable: false),
+                    slug = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    movie_id = table.Column<int>(type: "int", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    deleted = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_episodes", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_episodes_movies_movie_id",
+                        column: x => x.movie_id,
+                        principalTable: "movies",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_casts_movie_id",
+                table: "casts",
+                column: "movie_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_episodes_movie_id",
+                table: "episodes",
+                column: "movie_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "casts");
+
+            migrationBuilder.DropTable(
+                name: "episodes");
+
+            migrationBuilder.DropTable(
+                name: "actors");
+
+            migrationBuilder.DropTable(
+                name: "movies");
+        }
+    }
+}
